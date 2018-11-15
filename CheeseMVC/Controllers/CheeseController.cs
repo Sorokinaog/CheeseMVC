b8e6165d3611117b744ce748776c440853891111
Olga Sorokina <sorokinaog@icloud.com>
2018-11-14 23:15:45 -0500

    Studio4

---
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+
+        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.cheeses = Cheeses;
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name, string description)
+        {
+            // Add the new cheese to my existing cheeses
+            Cheeses.Add(name, description);
+            return Redirect("/Cheese");
+        }
+
+        public IActionResult Delete()
+        {
+            ViewBag.cheeses = Cheeses;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Delete")]
+        public IActionResult DeleteCheese(string[] cheese)
+        {
+            foreach (string name in cheese)
+            {
+                Cheeses.Remove(name);
+            }
+            return Redirect("/Cheese");
+
+        }
+    }
+}
